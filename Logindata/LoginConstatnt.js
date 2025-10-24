aa35f9d3a7885b9778fb761e91e9e8588f3e6aea
Rajeev Kumar <XP.Rajeev.Kumar@inovalon.global>
2025-10-24 17:15:38 +0530

    Fix K6 load testing issues - 100% success rate achieved

--- Logindata/LoginConstatnt.js
+++ Logindata/LoginConstatnt.js
@@ -4,4 +4,4 @@ export const userName   = "";
 export const password   =  "";
 export const sessionUrl =  "https://cm.claimsmgmt-qual.aws53.inovalon.global/mdonline/SessionTransfer/sessiontransfer.asp?Path=MaintenanceSupport/MyAccount&ToMvc=True";
 export const sessionTransferUrl =  "https://wapp-cm.claimsmgmt-qual.aws53.inovalon.global/mdonline/SessionTransfer/sessiontransfer.asp?Path=Actions/Home&ToMvc=True";
-export const sessionReportUrl =  "https://cm.claimsmgmt-qual.aws53.inovalon.global/APC/Reporting/SessionResults";
+export const sessionReportUrl =  "https://wapp-cm.claimsmgmt-qual.aws53.inovalon.global/APC/Reporting/SessionResults";
